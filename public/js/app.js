03df2f228d7bd5c2169cd17261c5068bcc31d996
Joonas Ulmanen <joonas.ulmanen@futurice.com>
2016-12-05 15:55:20 +0200

    Working multiple drones showing

--- public/js/app.js
+++ public/js/app.js
@@ -8,10 +8,9 @@ var DRONE_STATUS = {
     "RETURNING": 5
   };
 
-angular.module('airlyApp', ['ngGeolocation'])
+angular.module('airlyApp', ['uiGmapgoogle-maps'])
 
-
-.controller('shopControl', ['$scope', '$http', '$interval', '$geolocation', function($scope, $http, $interval){
+.controller('shopControl', ['$scope', '$http', '$interval', function($scope, $http, $interval){
   var self = this;
 
   self.drones = {};
@@ -27,10 +26,8 @@ angular.module('airlyApp', ['ngGeolocation'])
 
 }])
 
-.controller('droneControl', ['$scope', '$geolocation', '$timeout', '$http', function($scope, $geolocation, $timeout, $http){
-
+.controller('droneControl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http){
   var self = this;
-
   self.properties = {name: 'Asd', status: DRONE_STATUS.OFF, position: {}};
 
   function update2() {
@@ -41,7 +38,7 @@ angular.module('airlyApp', ['ngGeolocation'])
         window.navigator.geolocation.getCurrentPosition(
             function (position) {
                 console.log(position);
-                $scope.position = {lat: position.coords.latitude, lon: position.coords.longitude};
+                $scope.position = {lat: position.coords.latitude, lng: position.coords.longitude};
 
                 self.properties.position = $scope.position;
                 $http.post('/droneposition', self.properties)
@@ -61,4 +58,73 @@ angular.module('airlyApp', ['ngGeolocation'])
   }
   update2();
 
+}])
+
+.controller('userControl',['$scope', '$http', '$timeout', 'uiGmapGoogleMapApi', function($scope, $http, $timeout, uiGmapGoogleMapApi){
+  var self = this;
+  var droneImage = 'https://cdn2.iconfinder.com/data/icons/crystalproject/Open-Office-Icons/stock_draw-circle-16.png';
+  self.drones = {};
+  $scope.map = {
+    center: {latitude: 60.10, longitude: 25.0},
+    zoom: 6,
+    events: {
+      tilesloaded: function (map) {
+          $scope.$apply(function () {
+            self.mapInst = map;
+          });
+      }
+    }
+  };
+
+  function update2() {
+    console.log("updated?");
+    if (!window.navigator.geolocation) {
+      console.log('Geolocation not supported.');
+    } else {
+        window.navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log(position);
+
+                $scope.position = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+                uiGmapGoogleMapApi.then(
+                  function(inst){
+                    self.mapInst.setCenter($scope.position);
+                    $http.get('/dronestatus')
+                    .then(function(res){
+                      for(name in res.data){
+                        if(!self.drones[name]){
+                          self.drones[name] = res.data[name];
+                        } else {
+                          self.drones[name].position = res.data[name].position;
+                        }
+                        var drone = self.drones[name];
+                        if(!drone.marker){
+                          console.log("New marker!");
+                          drone.marker = new inst.Marker({
+                            position: drone.position,
+                            map: self.mapInst,
+                            icon: droneImage
+                          });
+                        } else {
+                          console.log("Old marker!");
+                          drone.marker.setPosition(drone.position);
+                        }
+                      }
+                    }, function(err){
+                      console.log(err);
+                    });
+                  }
+                );
+
+
+                $timeout(update2, 2000);
+            },
+            function (err) {
+                console.log(err);
+                $timeout(update2, 2000);
+            });
+    }
+  }
+  update2();
 }]);
